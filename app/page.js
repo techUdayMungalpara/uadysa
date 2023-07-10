4fb45c68f7152af45bf9f5adc4fc4f7421cbe454
Uday Mungalpara <techudaymungalpara@gmail.com>
2023-07-10 17:10:29 +0530

    jpeg

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export const metadata = {
     siteId: "1467726470533754880",
     creator: "@nextjs",
     images: {
-      url: "https://uadysa-techudaymungalpara.vercel.app/ikiotw.jpg",
+      url: "https://uadysa-techudaymungalpara.vercel.app/ikiotw.jpeg",
       alt: "Next.js Logo",
     },
 
